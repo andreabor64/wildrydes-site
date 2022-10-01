a4deb7a95877824de6bedf0a007b892ff0102c41
Borghi Andrea <07005521@telecomitalia.it>
2022-10-01 20:00:59 +0200

    Cognito1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-3_F5N8wQ9M4', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7kfgol3k1iqqhh14fidv7fhvaq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-3_6qqwBV3jv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ce0f52e3daabraoaacjc83km0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
