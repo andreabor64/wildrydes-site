b708b1074dfc6bda9627d742738cc63f1f08fb90
Borghi Andrea <07005521@telecomitalia.it>
2022-10-03 08:38:18 +0200

    Configure API invokeURL

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_6qqwBV3jv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3ce0f52e3daabraoaacjc83km0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3ce0f52e3daabraoaacjc83km0', // e.g. 3ce0f52e3daabraoaacjc83km0
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://huup7765g2.execute-api.eu-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
