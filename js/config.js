ffc9852dcf9633608cc5f533e26d5a79ee302f68
Borghi Andrea <07005521@telecomitalia.it>
2022-10-04 14:33:35 +0200

    Auth3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-3_VWAZYOJwz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '44342p4n1k06b5fmb608sq0n9a', // e.g. 3ce0f52e3daabraoaacjc83km0
+        userPoolId: 'eu-west-3_kg0t0Ytas', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7lich57mpons5nehm38l5mrliu', // e.g. 3ce0f52e3daabraoaacjc83km0
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
