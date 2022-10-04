4ef536cb5e8a4252eeac017b3121057cf3cc8504
Borghi Andrea <07005521@telecomitalia.it>
2022-10-04 12:18:47 +0200

    Auth

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-3_6qqwBV3jv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3ce0f52e3daabraoaacjc83km0', // e.g. 3ce0f52e3daabraoaacjc83km0
+        userPoolId: 'eu-west-3_VWAZYOJwz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44342p4n1k06b5fmb608sq0n9a', // e.g. 3ce0f52e3daabraoaacjc83km0
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
